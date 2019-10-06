c677f9eded3cbd7f93cf9ecceef501f5887af20b
Justina Kuchta <justyna.kuchta@gmail.com>
2019-10-05 20:59:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, num) {
+    //returns an array of all matching drivers
+    //returns an empty array if there is no match
+    return drivers.filter(driver => driver.revenue > num);
+}
+
+function driverNamesWithRevenueOver(drivers, num) {
+    let matching =  driversWithRevenueOver(drivers, num);
+    return matching.map(driver => driver.name);
+}
+
+//WHY DIDN'T 'key of pair' WORK?
+function exactMatch(drivers, pair) {
+    //This function takes an array of driver objects as the first argument 
+    //and a JS object that specifies an attribute and corresponding value. 
+    //For example, exactMatch(drivers, { revenue: 3000 })
+    return drivers.filter(function(driver) {
+        for(const key in pair) {
+            return driver[key] === pair[key]
+        };
+    })
+}
+
+function exactMatchToList(drivers, pair) {
+    let matching = exactMatch(drivers, pair);
+    return matching.map(driver => driver.name);
+}
